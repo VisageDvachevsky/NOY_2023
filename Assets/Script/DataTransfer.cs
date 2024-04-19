5aa9e08e00314efeaaeebeac14ce0fdd7f988dae
Visage <82640121+VisageDvachevsky@users.noreply.github.com>
2024-04-19 18:39:50 +0300

    govno

--- Assets/Script/DataTransfer.cs
+++ Assets/Script/DataTransfer.cs
@@ -5,29 +5,11 @@ using UnityEngine.SceneManagement;
 public class DataTransfer : MonoBehaviour
 {
     private float g;
+    private float mass;
 
     public void OnButtonClick()
     {
-        GameObject[] radiusInputs = GameObject.FindGameObjectsWithTag("RadiusInput");
-        float radius = 0f;
-        foreach (GameObject inputField in radiusInputs)
-        {
-            TMP_InputField tmpInput = inputField.GetComponent<TMP_InputField>();
-            if (tmpInput != null)
-            {
-                if (float.TryParse(tmpInput.text, out float parsedRadius))
-                {
-                    radius = parsedRadius;
-                }
-                else
-                {
-                    Debug.LogError("Failed to parse radius value!");
-                }
-            }
-        }
-
         GameObject[] massInputs = GameObject.FindGameObjectsWithTag("MassInput");
-        float mass = 0f;
         foreach (GameObject inputField in massInputs)
         {
             TMP_InputField tmpInput = inputField.GetComponent<TMP_InputField>();
@@ -35,7 +17,7 @@ public class DataTransfer : MonoBehaviour
             {
                 if (float.TryParse(tmpInput.text, out float parsedMass))
                 {
-                    mass = parsedMass;
+                    mass = parsedMass * 1e-21f;
                 }
                 else
                 {
@@ -44,11 +26,11 @@ public class DataTransfer : MonoBehaviour
             }
         }
 
-
-        const float G = 6.67430e-11f; 
-        g = G * (mass / (radius * radius));
+        const float G = 6.67430e-11f;
+        g = G * mass;
 
         PlayerPrefs.SetFloat("Gravity", g);
+        PlayerPrefs.SetFloat("Mass", mass);
 
         SceneManager.LoadScene("Custom");
         DontDestroyOnLoad(gameObject);
--- Assets/Script/SetGravity.cs
+++ Assets/Script/SetGravity.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
+using TMPro;
 
 public class SetGravity : MonoBehaviour
 {
     void Start()
     {
-        float gravity = PlayerPrefs.GetFloat("Gravity", 9.81f); 
-
+        float gravity = PlayerPrefs.GetFloat("Gravity", 9.81f);
         Physics.gravity = new Vector3(0, -gravity, 0);
+
+        float mass = PlayerPrefs.GetFloat("Mass", 0f); 
+        float radius = PlayerPrefs.GetFloat("Radius", 0f); 
+
+        TMP_Text gText = GameObject.FindGameObjectWithTag("g_text").GetComponent<TMP_Text>();
+        TMP_Text massText = GameObject.FindGameObjectWithTag("mass_text").GetComponent<TMP_Text>();
+        TMP_Text radiusText = GameObject.FindGameObjectWithTag("radius_text").GetComponent<TMP_Text>();
+
+        gText.text = "g: " + gravity.ToString("F2") + " м/с^2";
+        massText.text = "Масса: " + mass.ToString("F2") + " секстильонов тонн";
+        radiusText.text = "Радиус: " + radius.ToString("F2") + " км";
     }
 }
